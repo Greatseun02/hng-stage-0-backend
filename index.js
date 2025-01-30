fbe1db693e7d770214658fb41074b9060b214fa5
Goodnews Adewole <goodnews.adewole@example.com>
2025-01-30 11:30:26 +0100

    pushing

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+app.use(cors());
+
+app.get(
+    "/", (req, res)=>{
+        const currentISODate = new Date().toISOString();
+        const email = "goodnews.adewole9@gmail.com";
+        const github = "https://github.com/Greatseun02/hng-stage-0-backend";
+
+        const data = {
+            "email":email,
+            "current_datetime": currentISODate,
+            "github_url": github
+        }
+
+        res.json(
+            data
+        )
+    }
+)
+
+
+app.listen(process.env.PORT || 3000, ()=>{console.log("Server Started")})
